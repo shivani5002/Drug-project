4b23480180adc331fede89ddab520a8662c94d66
Shivani <shivani18905@gmail.com>
2025-05-24 10:05:51 +0530

    after modifying mongo url

--- mongo.js
+++ mongo.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017'; // Replace with your MongoDB URI
+//const uri = 'mongodb://localhost:27017'; // Replace with your MongoDB URI
+const uri = 'mongodb+srv://shivani18905:shivani12345@cluster0.vaezoqg.mongodb.net/genai_medical?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri);
 
 async function connect() {
