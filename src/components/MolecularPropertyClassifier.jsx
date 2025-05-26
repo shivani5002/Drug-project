368a7f6724e55ea1b52af3cfb980bd9c3e8d09d6
Shivani <shivani18905@gmail.com>
2025-05-26 20:27:45 +0530

    after updation

--- src/components/MolecularPropertyClassifier.jsx
+++ src/components/MolecularPropertyClassifier.jsx
@@ -22,6 +22,7 @@ function App() {
         headers: {
           'Content-Type': 'application/json',
         },
+         credentials: 'include', 
         body: JSON.stringify({ smiles }),
       });
       
